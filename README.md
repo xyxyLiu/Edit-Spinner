ed92a46a8eb05a731bb41e2f29a3d5b1453af2ea
liuxiaoyang <liuxiaoyang01@baidu.com>
2016-02-11 14:32:18 +0800

    change to  EditText implementation

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # EditableSpinner
-A Editable Spinner for Android
+An EditText which supports spinner FOR `API LEVEL >= 11`
+
+<div><img src='https://github.com/xyxyLiu/EditableSpinner/blob/master/art/demo.gif' width="400px" style='border: #f1f1f1 solid 1px'/></div>
+
+[Download Demo APK](https://github.com/xyxyLiu/EditableSpinner/releases/download/1.0/EditableSpinner-Demo-1.0.apk)
+
+## Usage
+add EditableSpinner in xml
+```xml
+<com.reginald.editablespinner.EditableSpinner
+        android:id="@+id/editable_spinner"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        app:dropDownDrawable="@drawable/spinner_icon"
+        app:dropDownDrawableSpacing="15dp"
+        app:dropDownDrawableWidth="25dp"
+        app:dropDownDrawableHeight="25dp"
+        />
+```
+
+setup datas using ListAdapter
+```java
+        mEditableSpinner = (EditableSpinner) findViewById(R.id.editable_spinner);
+        ListAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+        getResources().getStringArray(R.array.your_string_array));
+        mEditableSpinner.setAdapter(adapter);
+```
+
+key properties:
+
+|     attr    	|  required/optional(default value)  	|                         meaning                         	|
+|:-----------:	|:---------:	|:----------------------------------------------------:	|
+|  dropDownDrawable  	|   required    | dropdown drawable shown on the right of the EditText  	|
+|   dropDownDrawableWidth   	|     optional, default using drawable intrinsic width     	|         width of dropdown drawable        	|
+| dropDownDrawableHeight 	| optional, default using drawable intrinsic height     	|    height of dropdown drawable     	  	|
+| dropDownDrawableSpacing      | optional, default is 0 	|          the spacing between edittext area and dropdown drawable area    |
+
+## License
+
+    Copyright 2016 xyxyLiu
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
--- sample/src/main/java/com/reginald/editablespinner/sample/DemoActivity.java
+++ sample/src/main/java/com/reginald/editablespinner/sample/DemoActivity.java
@@ -1,14 +1,25 @@
 package com.reginald.editablespinner.sample;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.PopupWindow;
 
 import com.reginald.editablespinner.EditableSpinner;
 
 public class DemoActivity extends Activity {
 
-    EditableSpinner mEditableSpinner;
+    private static final String TAG = "DemoActivity";
+
+    EditableSpinner mEditableSpinner1;
+    EditableSpinner mEditableSpinner2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +28,79 @@ public class DemoActivity extends Activity {
     }
 
     private void initViews() {
-        mEditableSpinner = (EditableSpinner) findViewById(R.id.editable_spinner);
-        mEditableSpinner.setDrawable(getResources().getDrawable(R.drawable.picker), 60, 60);
-        mEditableSpinner.setRightDrawablePadding(50);
-        mEditableSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, getResources().getStringArray(R.array.edits_array)));
+        mEditableSpinner1 = (EditableSpinner) findViewById(R.id.editable_spinner_1);
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.edits_array_1));
+        mEditableSpinner1.setAdapter(adapter);
+
+//        mEditableSpinner1.setDropDownDrawable(getResources().getDrawable(R.drawable.picker), 60, 60);
+//        mEditableSpinner1.setDropDownDrawableSpacing(50);
+//        mEditableSpinner1.setDropDownAnimationStyle(R.style.CustomPopupAnimation);
+//        mEditableSpinner1.setDropDownBackgroundResource(R.drawable.your_custom_dropdown_bkg);
+        mEditableSpinner1.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                Log.d(TAG, "mEditableSpinner1 popup window dismissed!");
+            }
+        });
+
+        mEditableSpinner1.setOnShowListener(new EditableSpinner.OnShowListener() {
+            @Override
+            public void onShow() {
+                hideSoftInputPanel();
+            }
+        });
+
+
+        final String[] stringArray2 = getResources().getStringArray(R.array.edits_array_2);
+        mEditableSpinner2 = (EditableSpinner) findViewById(R.id.editable_spinner_2);
+        mEditableSpinner2.setDropDownDrawable(getResources().getDrawable(R.drawable.spinner), 25, 25);
+        mEditableSpinner2.setDropDownDrawableSpacing(50);
+        mEditableSpinner2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, stringArray2));
+        mEditableSpinner2.setItemConverter(new EditableSpinner.ItemConverter() {
+            @Override
+            public String convertItemToString(Object selectedItem) {
+                if (selectedItem.toString().equals(stringArray2[stringArray2.length - 1])) {
+                    return "";
+                } else {
+                    return selectedItem.toString();
+                }
+            }
+        });
+
+        mEditableSpinner2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick() position = " + position);
+                if (position == stringArray2.length - 1) {
+                    showSoftInputPanel(mEditableSpinner2);
+                }
+            }
+        });
+
+        mEditableSpinner2.setOnShowListener(new EditableSpinner.OnShowListener() {
+            @Override
+            public void onShow() {
+                hideSoftInputPanel();
+            }
+        });
+
+        mEditableSpinner2.selectItem(0);
     }
 
 
+    private void hideSoftInputPanel() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(mEditableSpinner1.getWindowToken(), 0);
+        }
+    }
+
+    private void showSoftInputPanel(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.showSoftInput(view, 0);
+        }
+    }
 
 }
